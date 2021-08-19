5360091f2b9450a0c6bad84c1e464293d798cb67
liyuxin <liyuxin_206@163.com>
2021-08-19 16:33:32 +0800

    Finish the A Simple Example.

--- A Simple Example/test1.py
+++ A Simple Example/test1.py
@@ -10,29 +10,34 @@ import pyro.distributions as dist
 pyro.set_rng_seed(101)
 
 
+# weight 是一个以guess为均值，1为标准差的正太分布
+# measurement 是一个以weight为均值，0.75为标准差的正太分布（注，weight是个分布）
 def scale(guess):
     weight = pyro.sample("weight", dist.Normal(guess, 1.0))
     return pyro.sample("measurement", dist.Normal(weight, 0.75))
 
 
+# 用来拟合分布的分布
 def scale_parametrized_guide(guess):
     a = pyro.param("a", torch.tensor(guess))
     b = pyro.param("b", torch.tensor(1.))
     return pyro.sample("weight", dist.Normal(a, torch.abs(b)))
 
+# scale 这个分布的条件分布，条件为measurement = 9.5（注，P[scale|measurement = 9.5]）
 conditioned_scale = pyro.condition(scale, data={"measurement": torch.tensor(9.5)})
 
 guess = 8.5
 
+# 拟合方法设置
 pyro.clear_param_store()
 svi = pyro.infer.SVI(model=conditioned_scale,
                      guide=scale_parametrized_guide,
                      optim=pyro.optim.Adam({"lr": 0.003}),
                      loss=pyro.infer.Trace_ELBO())
 
-
+# 训练并保存中间结果
 losses, a, b = [], [], []
-num_steps = 2500
+num_steps = 8500
 for t in range(num_steps):
     losses.append(svi.step(guess))
     a.append(pyro.param("a").item())
@@ -52,9 +57,9 @@ plt.plot([0, num_steps], [9.14, 9.14], 'k:')
 plt.plot(a)
 plt.ylabel('a')
 
-plt.subplot(1,2,2)
+plt.subplot(1, 2, 2)
 plt.ylabel('b')
-plt.plot([0,num_steps],[0.6,0.6], 'k:')
+plt.plot([0, num_steps], [0.6, 0.6], 'k:')
 plt.plot(b)
 plt.tight_layout()
 plt.show()
